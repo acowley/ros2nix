d588a44f46e5d515dc6dae1a3fa8dfee7fd81b6f
Anthony Cowley <acowley@gmail.com>
2016-04-14 19:26:47 -0400

    Distinguish packages for which we have a hash
    We need a source hash to generate a Nix package, but the original ROS
    distro description does not include this. We now distinguish these two
    phases with appropriate types.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts, OverloadedStrings, TemplateHaskell #-}
 import Control.Applicative ((<|>), liftA2)
 import Control.Lens
 import Data.Maybe (catMaybes)
@@ -19,18 +19,26 @@ testFile = "/Users/acowley/Documents/Projects/Nix/Ros/indigo_perception_ws/src/a
 testDistro :: FilePath
 testDistro = "/Users/acowley/Documents/Projects/Nix/Ros/indigo_perception_ws/indigo_perception_ws.rosinstall"
 
-data Package = Package { localName :: Text
-                       , uri       :: Text
-                       , version   :: Text
-                       , sha256    :: Text }
-             deriving Show
+data RosPackage = RosPackage { _localName :: Text
+                             , _uri       :: Text
+                             , _version   :: Text }
+                  deriving Show
+makeClassy ''RosPackage
+
+data Package = Package { rosPackage' :: RosPackage
+                       , sha256     :: Text }
+               deriving Show
+makeLenses ''Package
+
+instance HasRosPackage Package where
+  rosPackage = lens rosPackage' (\(Package _ s) -> flip Package s)
 
 mkStr' :: Text -> NExpr
 mkStr' = mkStr DoubleQuoted
 
 fetchSrc :: Package -> NExpr
 fetchSrc pkg = mkApp (mkSym "fetchurl") $ mkNonRecSet
-               [ NamedVar (mkSelector "url") (mkStr' (uri pkg))
+               [ NamedVar (mkSelector "url") (mkStr' (view uri pkg))
                , NamedVar (mkSelector "sha256") (mkStr' (sha256 pkg)) ] 
 
 nixify :: Package -> [String] -> NExpr
@@ -38,30 +46,29 @@ nixify pkg deps = mkFunction args body
   where args = mkFormalSet $ zip ("stdenv" : map T.pack deps) (repeat Nothing)
         kv k v = NamedVar (mkSelector k) v
         body = mkApp (mkSym "stdenv.mkDerivation") $ mkNonRecSet
-               [ kv "name" (mkStr' (localName pkg))
-               , kv "version" (mkStr' (version pkg))
+               [ kv "name" (mkStr' (view localName pkg))
+               , kv "version" (mkStr' (view version pkg))
                , kv "src" (fetchSrc pkg)
                , kv "buildInputs" (mkList deps') ]
         deps' = map mkSym ["cmake", "pkgconfig"] ++ map (mkSym . T.pack) deps
 
-prefetch :: Package -> IO Package
+prefetch :: RosPackage -> IO Package
 prefetch pkg = do sha <- T.pack . init . view _2 <$>
                          readCreateProcessWithExitCode cp ""
-                  putStrLn $ "Prefetched "++ T.unpack (localName pkg)
-                  return pkg { sha256 = sha }
-  where url = T.unpack (uri pkg)
+                  putStrLn $ "Prefetched "++ T.unpack (view localName pkg)
+                  return $ Package pkg sha
+  where url = T.unpack (view uri pkg)
         cp = proc "nix-prefetch-url" [url]
 
 -- | Parse a @.rosinstall@ file extracting package information
-getPackages :: FilePath -> IO [Package]
+getPackages :: FilePath -> IO [RosPackage]
 getPackages f = aux <$> parseYamlFile f -- >>= mapM prefetch
   where aux y = catMaybes $ y ^.. each . key "tar" . to pkg
-        pkg :: YamlLight -> Maybe Package
-        pkg y = Package 
+        pkg :: YamlLight -> Maybe RosPackage
+        pkg y = RosPackage 
               <$> y ^? key "local-name" . _Yaml . to decodeUtf8
               <*> y ^? key "uri" . _Yaml . to decodeUtf8
               <*> y ^? key "version" . _Yaml . to decodeUtf8
-              <*> pure ""
 
 getDependencies :: String -> IOSLA (XIOState s) a String
 getDependencies f = readDocument [] f
