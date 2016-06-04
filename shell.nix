fc826ac89a255c1ef69b38046e4ce7be45cb84a9
Anthony Cowley <acowley@gmail.com>
2016-06-03 20:05:12 -0400

    Using intero for development

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@ let
 
   f = { mkDerivation, stdenv, base, containers, hxt, text, hnix, directory, lens
       , process, yaml-light, yaml-light-lens, temporary, filepath, logging
-      , optparse-applicative }:
+      , optparse-applicative, intero, cabal-install }:
       mkDerivation {
         pname = "ros2nix";
         version = "0.1.0.0";
@@ -16,13 +16,13 @@ let
         executableHaskellDepends = [ base hxt containers text hnix process
                                      lens yaml-light yaml-light-lens
                                      temporary directory filepath logging
-                                     optparse-applicative ];
+                                     optparse-applicative intero cabal-install ];
         description = "Build Nix definitions of a ROS distribution";
         license = stdenv.lib.licenses.bsd3;
       };
 
   haskellPackages = if compiler == "default"
-                       then pkgs.haskell.packages.lts-5_11
+                       then pkgs.myHaskellPackages
                        else pkgs.haskell.packages.${compiler};
 
   drv = haskellPackages.callPackage f {};
